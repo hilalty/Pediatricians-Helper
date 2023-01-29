5c396d453eb9d20d58afe4705b3d772b9fcc5c5c
Hilal Yilmaz <hilaltiftik@gmail.com>
2023-01-28 17:06:01 -0800

    organized and cleaned repetetive codes in server.js

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const viewController = require('./viewController.js')
 const dataController = require('./dataController.js')
 
--- server.js
+++ server.js
@@ -1,40 +1,22 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const path = require("path");
-
-
-const methodOverride = require("method-override");
-
 const PORT = 3000;
-
-const Patient = require("./models/patients.js");
 const patientController = require("./controllers/patientController");
 
-/**
- * Controller requires go here ⬇️
- */
+const db = require('./models/db')
+db.once('open', () => {
+  console.log('connected to mongo')
+})
 
-//--------------------------------
-
-const db = require("./models/db");
-db 
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
-
-const setupMiddleware = require("./middleware/setupMiddleware");
-
-setupMiddleware(app);
-
-
-/**
- * View engine
- */
 app.set("view engine", "jsx");
 app.engine("jsx", require("jsx-view-engine").createEngine());
+
+
+const setupMiddleware = require("./middleware/setupMiddleware");
+setupMiddleware(app);
+
 
 app.use("/patients", patientController);
 
@@ -42,7 +24,6 @@ app.get("/", (req, res) => {
   res.redirect("/patients/");
 });
 
-// Listen on the port
 app.listen(PORT, () => {
   console.log(`listening on port:${PORT} http://localhost:${PORT}/`);
 });
